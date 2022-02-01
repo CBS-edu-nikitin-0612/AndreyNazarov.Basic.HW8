444720d80c99d8fbab55aa7457a9eb7ef453c1de
Andrew <1@1.rugit config --global user.name Andrewcd F:\ITVDN\2 git config --global user.email 1@1.ru>
2022-02-01 17:30:41 +0300

    feat: Task5

---
+++ Task5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please type your date of birthday in format \"01-01-2000\" and press enter");
+            string input = Console.ReadLine();
+            DateTime birthdayDate;
+            if (DateTime.TryParse(input, out birthdayDate))
+            {
+                TimeSpan timeSpan = birthdayDate.AddYears(DateTime.Now.Year - birthdayDate.Year) - DateTime.Now;
+                Console.WriteLine($"Until your birthday left {timeSpan.Days} days");
+            }
+            else
+            {
+                Console.WriteLine("Date of your birthday had not parsed");
+            }
+        }
+    }
+}

31a9a2d81ef840356454ece1bdcb96e3f01ff8ab
Andrew <1@1.rugit config --global user.name Andrewcd F:\ITVDN\2 git config --global user.email 1@1.ru>
2022-02-01 16:53:51 +0300

    Add project files.

---
+++ Task2/MyClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Task2
+{
+    internal static class MyClass
+    {
+        public enum Color
+        {
+            Black = ConsoleColor.Black,
+            Red = ConsoleColor.Red,
+            Blue = ConsoleColor.Blue
+        }
+        public static void Print(string stroka, int color)
+        {
+            Console.ForegroundColor = (ConsoleColor)color;
+            Console.WriteLine(stroka);
+            Console.ResetColor();
+        }
+    }
+
+
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please use number of one of the folowing colors:");
+            MyClass.Color[] colors = (MyClass.Color[])Enum.GetValues(typeof(MyClass.Color));
+            for (int i = 0; i < colors.Length; i++)
+            {
+                Console.WriteLine($"{colors[i]} - {(int)colors[i]}");
+            }
+
+            var inputColor = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please type text");
+            var inputText = Console.ReadLine();
+
+            MyClass.Print(inputText, inputColor);
+        }
+    }
+}
